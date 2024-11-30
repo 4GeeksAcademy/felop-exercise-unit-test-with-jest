cd37e8c3e292304c14be2853aefc2f0a544b43bc
Felop2299 <pipe-220899@hotmail.com>
2024-11-30 05:03:11 +0000

    test

---
+++ app.js
@@ -0,0 +1,21 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+const fromDollarToYen = function(valueInDollar) {
+    
+    let valueInYen = (valueInDollar / 1.07) * 156.5;
+    
+    return valueInYen;
+};
+const fromYenToPound = function(valueInYen) {
+    
+    let valueInPound = (valueInYen / 156.5) * 0.87;
+   
+    return valueInPound;
+};
+
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound }
---
+++ test.js
@@ -0,0 +1,42 @@
+
+
+test("One euro should be 1.07 dollars", function() {
+    // Import the function from app.js
+    const { fromEuroToDollar } = require('./app.js');
+
+    // Use the function like its supposed to be used
+    const dollars = fromEuroToDollar(3.5);
+
+    // If 1 euro is 1.07 dollars, then 3.5 euros should be (3.5 * 1.07)
+    const expected = 3.5 * 1.07; 
+    
+    // This is the comparison for the unit test
+     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
+})
+
+test("One dollar should be 146.26 yen", function() {
+    // Import the function from app.js
+    const { fromDollarToYen } = require('./app.js');
+
+    // Use the function like its supposed to be used
+    const yen = fromDollarToYen(2);
+
+    
+    const expected = 2 * 146.26; 
+
+    // This is the comparison for the unit test
+    expect(fromDollarToYen(2)).toBeCloseTo(292.52, 2); 
+});
+test("One yen should be 0.00556 pounds", function() {
+    // Import the function from app.js
+    const { fromYenToPound } = require('./app.js');
+
+    // Use the function like its supposed to be used
+    const pounds = fromYenToPound(200);
+
+    
+    const expected = 200 * 0.0055; 
+
+    // This is the comparison for the unit test
+    expect(fromYenToPound(200)).toBeCloseTo(1.112, 3); 
+});
